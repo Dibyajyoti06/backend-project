a791edd8d41d177775cb6282482db127aedc84d9
Dibyajyoti06 <dibyajyotinaik79@gmail.com>
2024-03-09 21:28:09 +0530

    add healthcheck controller

---
+++ src/controller/healthcheck.controller.js
@@ -0,0 +1,9 @@
+const healthcheck = asyncHandler(async (req, res) => {
+  //TODO: build a healthcheck response that simply returns the OK status as json with a message
+
+  return res.status(200).json({
+    message: 'Everything is OK!',
+  });
+});
+
+module.exports = healthcheck;
